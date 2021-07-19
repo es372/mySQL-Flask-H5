f16e777e1451a4358cd95f6f9c965205c28ff7d4
es372 <es372@njit.edu>
2021-07-18 21:25:58 -0400

    Task: Trees Web Application

---
+++ app/app.py
@@ -0,0 +1,30 @@
+import simplejson as json
+from flask import Flask, request, Response, redirect
+from flask import render_template
+from flaskext.mysql import MySQL
+from pymysql.cursors import DictCursor
+
+
+app = Flask(__name__)
+mysql = MySQL(cursorclass=DictCursor)
+
+app.config['MYSQL_DATABASE_HOST'] = 'db'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_PORT'] = 3306
+app.config['MYSQL_DATABASE_DB'] = 'treesData'
+mysql.init_app(app)
+
+
+@app.route('/', methods = ['GET'])
+def index():
+    user = {'username': 'Trees Homework5'}
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM treesTable')
+    result = cursor.fetchall()
+    return render_template('index.html', user=user, trees=result)
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
