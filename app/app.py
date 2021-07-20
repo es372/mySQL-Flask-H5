3993b51ba1d15e52f62d7279aba96ab54ccc7bf1
es372 <es372@njit.edu>
2021-07-20 17:37:44 -0400

    Error: add endpoint 'POST'

--- app/app.py
+++ app/app.py
@@ -36,6 +36,9 @@ def view_tree_index(tree_value):
     result = cursor.fetchall()
     return render_template('view.html', tree=result[0])
 
+
+
+
 @app.route('/edit/<int:tree_value>', methods = ['GET'])
 def edit_tree_index(tree_value):
         cursor = mysql.get_db().cursor()
@@ -43,37 +46,15 @@ def edit_tree_index(tree_value):
         result = cursor.fetchall()
         return render_template('edit.html', tree=result[0])
 
-
-
 @app.route('/edit/<int:tree_value>', methods=['POST'])
 def save_tree_edit(tree_value):
     cursor = mysql.get_db().cursor()
-    update = '''UPDATE treesTable t SET value=%s, Girth_in=%s, Height_ft=%s, Volume_ft=%s WHERE value=%s'''
+    update = '''UPDATE treesTable SET value=%s, Girth_in=%s, Height_ft=%s, Volume_ft=%s WHERE value=%s'''
     cursor.execute(update, (request.form.get('value'), request.form.get('Girth_in'), request.form.get('Height_ft'),
              request.form.get('Volume_ft'), tree_value))
     mysql.get_db().commit()
     return redirect("/", code=302)
 
-
-
-@app.route('/api/v1/trees', methods=['GET'])
-def api_browse() -> str:
-    cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM treesTable')
-    result = cursor.fetchall()
-    json_result = json.dumps(result);
-    resp = Response(json_result, status=200, mimetype='application/json')
-    return resp
-
-
-@app.route('/api/v1/cities/<int:tree_value>', methods=['GET'])
-def api_retrieve(tree_value) -> str:
-    cursor = mysql.get_db().cursor()
-    cursor.execute('SELECT * FROM treesTable WHERE value=%s', tree_value)
-    result = cursor.fetchall()
-    json_result = json.dumps(result);
-    resp = Response(json_result, status=200, mimetype='application/json')
-    return resp
 
 
 @app.route('/trees/new', methods=['GET'])
@@ -89,6 +70,8 @@ def save_new_tree():
     mysql.get_db().commit()
     return redirect("/", code=302)
 
+
+
 @app.route('/delete/<int:tree_value>', methods=['POST'])
 def form_delete_post(tree_value):
     cursor = mysql.get_db().cursor()
@@ -96,6 +79,63 @@ def form_delete_post(tree_value):
     cursor.execute(sql_delete_query, tree_value)
     mysql.get_db().commit()
     return redirect("/", code=302)
+
+
+
+@app.route('/api/v1/trees', methods=['GET'])
+def api_browse() -> str:
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM treesTable')
+    result = cursor.fetchall()
+    json_result = json.dumps(result)
+    resp = Response(json_result, status=200, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/trees/<int:tree_value>', methods=['GET'])
+def api_retrieve(tree_value) -> str:
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM treesTable WHERE value=%s', tree_value)
+    result = cursor.fetchall()
+    json_result = json.dumps(result)
+    resp = Response(json_result, status=200, mimetype='application/json')
+    return resp
+
+@app.route('/api/v1/trees/<int:tree_value>', methods=['PUT'])
+def api_edit(tree_value) -> str:
+    cursor = mysql.get_db().cursor()
+    content = request.json
+    inputData = (content['value'], content['Girth_in'], content['Height_ft'],
+                 content['Volume_ft'], tree_value)
+    sql_update_query = '''UPDATE treesTable SET value=%s, Girth_in=%s, Height_ft=%s, Volume_ft=%s WHERE value=%s'''
+    cursor.execute(sql_update_query, inputData)
+    mysql.get_db().commit()
+    resp = Response(status=200, mimetype='application/json')
+    return resp
+
+@app.route('/api/v1/trees', methods=['POST'])
+def api_add() -> str:
+    content = request.json
+    cursor = mysql.get_db().cursor()
+    insert = """INSERT INTO treesTable (value, Girth_in, Height_ft, Volume_ft) VALUES (%s, %s,%s, %s)"""
+    cursor.execute(insert, (content['value'], content['Girth_in'], content['Height_ft'],
+                 content['Volume_ft']))
+    mysql.get_db().commit()
+    resp = Response(status=201, mimetype='application/json')
+    return resp
+
+
+@app.route('/api/v1/trees/<int:tree_value>', methods=['DELETE'])
+def api_delete(tree_value) -> str:
+    cursor = mysql.get_db().cursor()
+    sql_delete_query = """DELETE FROM treesTable WHERE value = %s """
+    cursor.execute(sql_delete_query, tree_value)
+    mysql.get_db().commit()
+    resp = Response(status=200, mimetype='application/json')
+    return resp
+
+
+
 
 
 if __name__ == '__main__':
